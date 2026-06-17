164b42a4e511cf0b90d496590a37adbe62da9066
nobu09 <11857481+nobu09@users.noreply.github.com>
2026-06-18 08:40:26 +0900

    未使用のimportなどを削除

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 const GET_TODOS = gql`
@@ -16,7 +13,6 @@ const GET_TODOS = gql`
 `
 
 function App() {
-  const [count, setCount] = useState(0)
   const { loading, error, data } = useQuery(GET_TODOS)
 
   if (loading) return <p>読み込み中...</p>
